22b2b15765907fcc27f58701e9211522ef7f02cf
chrispittersl <christophsilvaa@gmail.com>
2021-01-14 18:06:54 -0300

    Estabelecer conexao com o banco de dados via PDO

---
+++ conexao.php
@@ -0,0 +1,9 @@
+<?php
+    try{
+        $pdo = new PDO("mysql:host=localhost;dbname=vip","root","",array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+        $pdo->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+
+    }catch(PDOException $e){
+        echo $e->getMessage();
+    }
+?>
