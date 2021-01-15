f7a2559052bbe021e9a94faf925ad8c257306c40
chrispittersl <christophsilvaa@gmail.com>
2021-01-15 00:56:44 -0300

    Adicionar empréstimo

---
+++ add.php
@@ -0,0 +1,31 @@
+<?php
+    $livro = $_POST["livro"];
+    $stmt = $pdo->prepare("SELECT * FROM livro where titulo like '$livro'");
+    $rows = $stmt->rowCount();
+    $erro = 0;
+    if($rows > 0){
+        $associado = $_POST["associado"];
+        $stmt = $pdo->prepare("SELECT * FROM associado where nome like '$associado'");
+        $dados = $stmt->fetch(PDO::FETCH_ASSOC);
+        $id_associado = $dados['id'];
+        $rows = $stmt->rowCount();
+        if($rows > 0){
+            try{
+                $stmt = $pdo->prepare("UPDATE livro set id_associado = $id_associado where titulo like '$livro'");
+                $stmt->execute()
+            }
+            catch(Exception $e){
+                $erro = 1;
+            }
+        }else{
+            $erro = 1
+        }
+    }else{
+        $erro = 1;
+    }
+
+    if($erro == 0)
+        echo json_encode("sucesso");
+    else
+        echo json_encode("falha");
+?>
