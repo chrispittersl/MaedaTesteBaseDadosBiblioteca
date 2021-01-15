2a41babffc73681e14a1d40387d3a6672bd34e09
chrispittersl <christophsilvaa@gmail.com>
2021-01-14 23:40:40 -0300

    Bugs na tabela arrumado

--- index.php
+++ index.php
@@ -65,23 +65,26 @@
 					</thead>
 					<tbody>
 					<?php
-						$stmt = $pdo->prepare("SELECT L.titulo, Assoc.nome AS nome_associado, Au.nome AS nome_autor, 
-						Assun.descricao AS assunto_descricao, B.descricao AS biblio_descricao from livro AS L 
-						INNER JOIN associado AS Assoc ON Assoc.matricula = L.id_associado INNER JOIN biblioteca AS 
-						B ON B.id = L.id_biblioteca INNER JOIN producao AS P ON P.id_livro = isbn INNER JOIN categoria 
-						AS C ON C.id_livro = isbn INNER JOIN autor AS Au ON Au.id = P.id_autor INNER JOIN assunto AS Assun 
-						ON Assun.id = C.id_assunto");
+						$stmt = $pdo->prepare("SELECT titulo AS livro,Assoc.nome AS associado, 
+						B.descricao AS biblio,Assun.descricao AS assunto, 
+						Au.nome AS autor from livro 
+						INNER JOIN associado AS Assoc ON matricula = id_associado 
+						INNER JOIN biblioteca AS B ON B.id = id_biblioteca 
+						INNER JOIN categoria AS C ON C.id_livro = isbn 
+						INNER JOIN assunto AS Assun ON Assun.id = C.id_assunto
+						INNER JOIN producao AS P ON P.id_livro = isbn 
+						INNER JOIN autor AS Au ON Au.id = P.id_autor");
 						$stmt->execute();
                         while($row = $stmt->fetch(PDO::FETCH_ASSOC))
                         {
                 	?>
                           <tr>
-                              <td><?php echo $row['titulo'];?></td>
-                              <td><?php echo $row['nome_associado'];?></td>
-							  <td><?php echo $row['nome_autor'];?></td>
-							  <td><?php echo $row['assunto_descricao'];?></td>
-							  <td><?php echo $row['biblio_descricao'];?></td>
-							  <td><a href="" class="btn btn-danger btn-block" id="del_emprestimo">X</a></td>
+                              <td><?php echo $row['livro'];?></td>
+                              <td><?php echo $row['associado'];?></td>
+							  <td><?php echo $row['autor'];?></td>
+							  <td><?php echo $row['assunto'];?></td>
+							  <td><?php echo $row['biblio'];?></td>
+							  <td><a href="" class="btn btn-danger btn-block" class="livro" id="<?php echo $row['livro']; ?>">X</a></td>
                           </tr>
                 	<?php        
                         }
@@ -116,8 +119,9 @@
 	
 	<!-- Bootstrap core JavaScript -->
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+	<script src="js/script.js"></script>
 	<script type="text/javascript">
 		$(document).ready(function(){
 			const item_about = document.querySelector('#home-nav')
